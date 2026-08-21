a410f58d57e4b81bbb0746dd7ffff2689c7b0200
Ahmed Hafez <ahmedmohamedhafez715@gmail.com>
2026-08-21 16:12:53 +0300

    Finished Day 3 tasks

--- data-engineering/tests/test_foundation.py
+++ data-engineering/tests/test_foundation.py
@@ -1,23 +1,2 @@
-import requests, json
-
-def usgs():
-    site_id = "09504500"
-
-    url = f"https://waterservices.usgs.gov/nwis/iv/?format=json&sites={site_id}&siteStatus=all"
-    
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        
-        data = response.json()
-        
-        with open("api_response.json", "w") as file:
-            json.dump(data, file, indent=4)
-            
-        print("Data successfully saved to api_response.json")
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-
-if __name__ == "__main__":
-    usgs()
+def test_placeholder():
+    assert True
